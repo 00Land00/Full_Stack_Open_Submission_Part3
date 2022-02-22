2e28708c96e5538010194d5394136771403e9361
Lander Joshua Vitug <lander.vitug@gmail.com>
2022-02-21 21:45:32 -0500

    encoded password from special characters

--- mongo.js
+++ mongo.js
@@ -5,7 +5,7 @@ if (process.argv.length !== 5 || process.argv.length !== 3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = encodeURIComponent(process.argv[2])
 const name = process.argv[3]
 const number = process.argv[4]
 const url = 
