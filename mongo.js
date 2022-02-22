570173195ba6378ed7f153a8de702a2cd38c9a11
Lander Joshua Vitug <lander.vitug@gmail.com>
2022-02-21 21:42:50 -0500

    install mongoose and create js file to complete exercise 12 part 3

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length !== 5 || process.argv.length !== 3) {
+  console.log('Please provide the password, the name of the user and their phonenumber as an argument: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+const url = 
+  `mongodb+srv://landv:${password}@cluster0.txwh4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+const person = new Person({
+  name: name,
+  number: number
+})
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log("phonebook")
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
